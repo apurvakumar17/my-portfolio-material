6353553aabc4be05e1603d0af83042427f7a2e0f
apurvakumar17 <apurva7k@gmail.com>
2025-07-05 20:33:04 +0530

    Add Hero component
    - Implemented Hero component to display personal introduction and social media links.
    - Integrated SelfCode component to showcase coding skills in a styled format.
    - Added SVG logo for branding in the public directory.
    - Created personal-data utility to store user information and links.
    - Updated output.css for styling adjustments and utility classes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
     const [count, setCount] = useState(0);
 
     return (
-        <>
-          <Navbar />
-          <div className="mainpage"></div>
-        </>
+        <div className="App ">
+            <Navbar />
+            <div
+                className="mainpage min-h-screen relative mx-auto px-6 sm:px-12 lg:px-0 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem]"
+            >
+                <Hero />
+            </div>
+        </div>
     );
 }
 
---
+++ src/utils/personal-data.js
@@ -0,0 +1,18 @@
+export const personalData = {
+  name: "Apurva Kumar",
+  profile: '/profile.png',
+  designation: "Student",
+  description: "My name is Apurva Kumar. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'apurva7k@gmail.com',
+  phone: '+91 8178627106',
+  address: 'Sector-21, Rohini, New Delhi, India',
+  github: 'https://github.com/apurvakumar17',
+  facebook: 'https://www.facebook.com/apurvakumar17/',
+  instagram: 'https://www.instagram.com/apurvakumar17/',
+  linkedIn: 'https://www.linkedin.com/in/apurva-kumar-3a7496322/',
+  twitter: 'https://x.com/apurvacoder17',
+  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+  leetcode: "https://leetcode.com/u/apurvakumar17/",
+  devUsername: "apurvacoder17",
+  resume: "https://drive.google.com/file/d/1WqpttxjZmosacKQZm1z-P2rlU6kD-f-f/view?usp=sharing"
+}
